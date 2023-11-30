864b6caf1cef0665077566fe1dc91ec6adddcf9c
KDotWare <01001011Dot@duck.com>
2023-12-01 00:07:39 +0800

    Add contactus.html and extends layout in it

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/contactus", methods=["GET"])
+def contactus():
+    return render_template("contactus.html")
+
 if __name__ == "__main__":
     app.run()
