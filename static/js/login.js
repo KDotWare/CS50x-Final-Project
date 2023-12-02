97f20aed9f9deff34938ab663c74bed50c317a37
KDotWare <01001011dot@duck.com>
2023-12-02 23:05:09 +0800

    Add javascript input fields validation; Add xmlhttprequest post to login api;

--- static/js/login.js
+++ static/js/login.js
@@ -1,3 +1,60 @@
 const submit = document.querySelector("input[name=submit]");
 let email = document.querySelector("input[name=email]");
 let password = document.querySelector("input[name=password]");
+
+function FieldError(elementClass, message, display)
+{
+    elementClass.getElementsByTagName("span")[0].innerHTML = message;
+    elementClass.style.display = display;
+}
+
+function ValidateFields(email, password)
+{
+    let isValid = true;
+
+    if (email == "")
+    {
+        FieldError(document.querySelector(".emailAlert"), "email is empty!", "block");
+        isValid = false;
+    }
+
+    if (password == "")
+    {
+        FieldError(document.querySelector(".passwordAlert"), "password is empty!", "block");
+        isValid = false;
+    }
+}
+
+email.addEventListener("input", function(event)
+{
+    if (event.target.value != "")
+    {
+        FieldError(document.querySelector(".emailAlert"), "", "none");
+    }
+});
+
+password.addEventListener("input", function(event)
+{
+    if (event.target.value != "")
+    {
+        FieldError(document.querySelector(".passwordAlert"), "", "none");
+    }
+});
+
+submit.addEventListener("click", function()
+{
+    if (!ValidateFields(email.value, password.value))
+    {
+        return 1;
+    }
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "/login", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    let params = email.getAttribute("name") + "=" + email.value + "&"
+                + password.getAttribute("name") + "=" + password.value;
+
+    xhr.send(params);
+});
