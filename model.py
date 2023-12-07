c259059769f79ba967d4cd4dd3e584fdfc034838
KDotWare <01001011dot@duck.com>
2023-12-07 11:10:30 +0800

    Add user table as User class

--- model.py
+++ model.py
@@ -12,3 +12,13 @@ class ContactUs(Base):
     email = Column("email", String(256), nullable=False)
     message = Column("message", String(1024), nullable=False)
     created_date = Column("created_date", DateTime, nullable=False)
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column("id", Integer, autoincrement=True, primary_key=True)
+    first_name = Column("first_name", String(30), nullable=False)
+    last_name = Column("last_name", String(30), nullable=False)
+    email = Column("email", String(256), nullable=False)
+    password = Column("password", Text, nullable=False)
+    registered_date = Column("registered_date", DateTime, nullable=False)
