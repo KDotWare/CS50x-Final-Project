c87567ed11d894be46a8bcab5fec6c7d0d2822de
KDotWare <01001011dot@duck.com>
2023-12-07 10:53:37 +0800

    Add contactus table as ContactUs class

--- model.py
+++ model.py
@@ -1,4 +1,14 @@
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String, Text, DateTime
 
 class Base(DeclarativeBase):
     pass
+
+class ContactUs(Base):
+    __tablename__ = "contactus"
+
+    id = Column("id", Integer, autoincrement=True, primary_key=True)
+    full_name = Column("full_name", String(30), nullable=False)
+    email = Column("email", String(256), nullable=False)
+    message = Column("message", String(1024), nullable=False)
+    created_date = Column("created_date", DateTime, nullable=False)
